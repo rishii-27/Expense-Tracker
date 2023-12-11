c1ee4fc3b237825ae21f9fbadf7f1eab644edf01
Hrishikesh <hrishikesh.joshi1997@gmail.com>
2023-12-11 12:35:40 +0530

    App testing done

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders the SignUp component by default", () => {
+  render(<App />);
+  const signUpElement = screen.getByText(/Your Email/i);
+  expect(signUpElement).toBeInTheDocument();
+});
